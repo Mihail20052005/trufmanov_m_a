569344480c5cad6a13d74db95a48114f707386f4
Mihail20052005 <108936552+Mihail20052005@users.noreply.github.com>
2023-04-29 20:34:03 +0300

    add matrixs lib

--- trufmanov_m_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
+++ trufmanov_m_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
@@ -1,25 +1,39 @@
+#include <stdexcept>
 #include <cstddef>
-#include "iosfwd"
+#include <vector>
+#include <utility>
+#include <tuple>
 
 class MatrixS {
+public:
+    using SizeType = std::tuple<std::ptrdiff_t, std::ptrdiff_t>;
+
+public:
+    explicit MatrixS(const SizeType& size = { 0, 0 });
+    MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n);
+    ~MatrixS();
+    MatrixS(const MatrixS& other);
+    MatrixS& operator=(const MatrixS& other);
+    int& at(const SizeType& elem);
+    const int& at(const SizeType& elem) const;
+    int& at(const std::ptrdiff_t i, const std::ptrdiff_t j);
+    const int& at(const std::ptrdiff_t i, const std::ptrdiff_t j) const;
+    void resize(const SizeType& new_size);
+    void resize(const std::ptrdiff_t i, const std::ptrdiff_t j);
+    const SizeType& ssize() const noexcept;
+
+    /**
+     * \return количество строк в матрице (m)
+     */
+    std::ptrdiff_t nRows() const noexcept;
+
+    /**
+     * \return количество столбцов в матрице (n)
+     */
+    std::ptrdiff_t nCols() const noexcept;
+
 private:
-	MatrixS();
-	MatrixS(int rows, int cols);
-	MatrixS(const MatrixS& rhs);
-	~MatrixS() = default;
-	MatrixS& operator=(const MatrixS& rhs);
-
-
-	const int& at(int index_row, int index_col) const;
-	int& at(int index_row, int index_col);
-	const int cols() const;
-	const int rows() const;
-	void resize(int rows, int cols);
-
-private:
-	int* data = nullptr;
-	int col = 0;
-	int row = 0;
+    std::ptrdiff_t m_{};
+    std::ptrdiff_t n_{};
+    std::vector<int> data_{};
 };
-
-bool outOfRangeCheck(int index_row, int index_col, int row, int col);
--- trufmanov_m_a/prj.lab/matrixs/matrixs.cpp
+++ trufmanov_m_a/prj.lab/matrixs/matrixs.cpp
@@ -1,59 +1,92 @@
-#include <matrixs/matrixs.hpp>
-#include <cstddef>
-#include <stdexcept>
-#include "iosfwd"
-#include "utility"
+//
+// Created by kozin on 15.03.2023.
+//
+#include "matrixs/matrixs.hpp"
 
-MatrixS::MatrixS(int new_rows, int new_cols):row(new_rows),
-col(new_cols){
-	data = new int[row * col];
-	for(int i = 0; i < row * col; i++){
-		data[i] = 0;
-	}
+
+MatrixS::MatrixS(const SizeType& size) : m_(std::get<0>(size)), n_(std::get<1>(size)), data_(m_* n_) {}
+
+MatrixS::MatrixS(const std::ptrdiff_t m, const std::ptrdiff_t n) : m_(m), n_(n) {
+    if (m <= 0 || n <= 0) {
+        throw std::invalid_argument("Invalid matrix size");
+    }
+    data_.resize(m * n);
 }
+
+MatrixS::~MatrixS() = default;
+
+MatrixS::MatrixS(const MatrixS& other) : m_(other.m_), n_(other.n_), data_(other.data_) {}
 
 MatrixS& MatrixS::operator=(const MatrixS& other) {
-	delete[] data;
-	row = other.rows();
-	col = other.cols();
-	data = new int[row * col];
-	for (int i = 0; i < row * col; i++) {
-		data[i] = other.at(i / col, i % col);
-	}
-}
-const int& MatrixS::at(int index_row, int index_col) const{
-	if (!outOfRangeCheck) {
-		throw std::invalid_argument("Index out of range");
-	}
-	else {
-		return data[col * index_col + index_row];
-	}
+    if (this != &other) {
+        m_ = other.m_;
+        n_ = other.n_;
+        data_ = other.data_;
+    }
+    return *this;
 }
 
-int& MatrixS::at(int index_row, int index_col) {
-	if (!outOfRangeCheck) {
-		throw std::invalid_argument("Index out of range");
-	}
-	else {
-		return data[col * index_col + index_row];
-	}
+int& MatrixS::at(const SizeType& elem) {
+    if (std::get<0>(elem) < 0 || std::get<0>(elem) >= m_ || std::get<1>(elem) < 0 || std::get<1>(elem) >= n_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[std::get<0>(elem) * n_ + std::get<1>(elem)];
 }
 
-const int MatrixS::cols() const{
-	return col;
+const int& MatrixS::at(const SizeType& elem) const {
+    if (std::get<0>(elem) < 0 || std::get<0>(elem) >= m_ || std::get<1>(elem) < 0 || std::get<1>(elem) >= n_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[std::get<0>(elem) * n_ + std::get<1>(elem)];
 }
 
-const int MatrixS::rows() const{
-	return row;
+int& MatrixS::at(const std::ptrdiff_t i, const std::ptrdiff_t j) {
+    if (i < 0 || i >= m_ || j < 0 || j >= n_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[i * n_ + j];
 }
 
-bool outOfRangeCheck(int index_row, int index_col, int row, int col) {
-	if (index_row * index_col <= 0 || index_row > row 
-		|| index_col > col || index_col < 0) {
-		return false;
-	
-	}
-	return true;
-
-
+const int& MatrixS::at(const std::ptrdiff_t i, const std::ptrdiff_t j) const {
+    if (i < 0 || i >= m_ || j < 0 || j >= n_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[i * n_ + j];
 }
+
+void MatrixS::resize(const SizeType& new_size) {
+    if (std::get<0>(new_size) <= 0 || std::get<1>(new_size) <= 0) {
+        throw std::invalid_argument("Invalid matrix size");
+    }
+    std::vector<int> new_data(std::get<0>(new_size) * std::get<1>(new_size));
+    for (std::ptrdiff_t i = 0; i < std::min(m_, std::get<0>(new_size)); ++i) {
+        for (std::ptrdiff_t j = 0; j < std::min(n_, std::get<1>(new_size)); ++j) {
+            new_data[i * std::get<1>(new_size) + j] = at({ i, j });
+        }
+    }
+    m_ = std::get<0>(new_size);
+    n_ = std::get<1>(new_size);
+    data_ = std::move(new_data);
+}
+
+void MatrixS::resize(const std::ptrdiff_t i, const std::ptrdiff_t j) {
+    if (i <= 0 || j <= 0) {
+        throw std::invalid_argument("Invalid matrix size");
+    }
+    m_ = i;
+    n_ = j;
+    data_.resize(m_ * n_);
+}
+
+const MatrixS::SizeType& MatrixS::ssize() const noexcept {
+    return { m_, n_ };
+}
+
+ptrdiff_t MatrixS::nRows() const noexcept {
+    return m_;
+}
+
+ptrdiff_t MatrixS::nCols() const noexcept {
+    return n_;
+}
+
